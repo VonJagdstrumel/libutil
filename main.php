cb5b1f4d8843179268e90d4cdf96bc01ce3a6507
Steve A. Forget <VonJagdstrumel@users.noreply.github.com>
2015-05-14 20:42:43 +0200

    Added example script

--- main.php
+++ main.php
@@ -2,5 +2,22 @@
 
 require_once 'vendor/autoload.php';
 
-use ArgumentHandler;
+use LibArg\ArgumentManager;
+use LibArg\ArgumentDefinition;
 
+$one = new ArgumentDefinition('a', 'append', '', 'Force append some shit in there');
+$two = new ArgumentDefinition('r', 'recursive', ':', 'Do that shit recursively');
+$three = new ArgumentDefinition('c', 'color', '::', 'Color all the shit');
+$four = new ArgumentDefinition('s', '', '', 'Split the shit');
+$five = new ArgumentDefinition('', 'quiet', '', 'Don\'t print any shit');
+
+$manager = new ArgumentManager('[OPTIONS]', 'Hahaha! Something to test?');
+$manager->addArgumentDefinition($one);
+$manager->addArgumentDefinition($two);
+$manager->addArgumentDefinition($three);
+$manager->addArgumentDefinition($four);
+$manager->addArgumentDefinition($five);
+
+print $manager->generateHelp();
+
+var_dump($manager->getReceivedArgumentList());
