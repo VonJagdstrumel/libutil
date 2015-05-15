637807b60ff74e407e7f1ee07d49e8a77a884b3b
Steve A. Forget <VonJagdstrumel@users.noreply.github.com>
2015-05-15 02:25:15 +0200

    Updated example script with bogus calls

--- main.php
+++ main.php
@@ -5,11 +5,11 @@ require_once 'vendor/autoload.php';
 use LibArg\ArgumentManager;
 use LibArg\ArgumentDefinition;
 
-$one = new ArgumentDefinition('a', 'append', '', 'Force append some shit in there');
-$two = new ArgumentDefinition('r', 'recursive', ':', 'Do that shit recursively');
-$three = new ArgumentDefinition('c', 'color', '::', 'Color all the shit');
-$four = new ArgumentDefinition('s', '', '', 'Split the shit');
-$five = new ArgumentDefinition('', 'quiet', '', 'Don\'t print any shit');
+$one = new ArgumentDefinition('a', 'append', ArgumentDefinition::FLAG_NO_VALUE, 'Force append some shit in there');
+$two = new ArgumentDefinition('r', 'recursive', ArgumentDefinition::FLAG_MANDATORY_VALUE, 'Do that shit recursively');
+$three = new ArgumentDefinition('c', 'color', ArgumentDefinition::FLAG_OPTIONAL_VALUE, 'Color all the shit');
+$four = new ArgumentDefinition('s', null, ArgumentDefinition::FLAG_NO_VALUE, 'Split the shit');
+$five = new ArgumentDefinition(null, 'quiet', ArgumentDefinition::FLAG_NO_VALUE, 'Don\'t print any shit');
 
 $manager = new ArgumentManager('[OPTIONS]', 'Hahaha! Something to test?');
 $manager->addArgumentDefinition($one);
@@ -21,3 +21,16 @@ $manager->addArgumentDefinition($five);
 print $manager->generateHelp();
 
 var_dump($manager->getReceivedArgumentList());
+
+//$u = new ArgumentDefinition('.', '.', '', '');
+//$u = new ArgumentDefinition('.', null, '', '');
+//$u = new ArgumentDefinition(null, '.', '', '');
+//$u = new ArgumentDefinition(null, null, '', '');
+
+//$u = new ArgumentDefinition('.', 'a', '', '');
+//$u = new ArgumentDefinition(null, 'a', '', '');
+
+//$u = new ArgumentDefinition('a', '.', '', '');
+//$u = new ArgumentDefinition('a', null, '', '');
+
+//$u = new ArgumentDefinition('a', 'a', '', '');
