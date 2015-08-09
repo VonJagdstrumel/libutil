403a14d8f8c38c3332b82ec350a9209a4772d338
Steve A. Forget <VonJagdstrumel@users.noreply.github.com>
2015-08-09 22:42:10 +0200

    Division by 0 detection

--- src/Helper/MathHelper.php
+++ src/Helper/MathHelper.php
@@ -13,9 +13,14 @@ class MathHelper
      * @param int $dividend
      * @param int $divisor
      * @return int
+     * @throws \InvalidArgumentException
      */
     public static function modulo($dividend, $divisor)
     {
+        if(!$divisor) {
+            throw new \InvalidArgumentException('Cannot divide by 0');
+        }
+
         return ($divisor + ($dividend % $divisor)) % $divisor;
     }
 }
